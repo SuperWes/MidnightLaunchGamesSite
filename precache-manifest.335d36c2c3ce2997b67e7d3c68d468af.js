210e70df5d74efdaeee1af4f585ffa09aef6cb5b
Wes Ehrlichman <wes.ehrlichman@asynchrony.com>
2020-09-11 11:49:47 -0500

    Updates

---
+++ precache-manifest.335d36c2c3ce2997b67e7d3c68d468af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d253bf094ec0505d57891fad6196b70",
+    "url": "/MidnightLaunchGamesSite/index.html"
+  },
+  {
+    "revision": "2f0c6ddc035d175e1da9",
+    "url": "/MidnightLaunchGamesSite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14ee6ff9d24561fbd7e7",
+    "url": "/MidnightLaunchGamesSite/static/js/2.631ba81c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MidnightLaunchGamesSite/static/js/2.631ba81c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0c6ddc035d175e1da9",
+    "url": "/MidnightLaunchGamesSite/static/js/main.10ffa501.chunk.js"
+  },
+  {
+    "revision": "fc03b6c1008ba34d9dea",
+    "url": "/MidnightLaunchGamesSite/static/js/runtime-main.7624f80d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MidnightLaunchGamesSite/static/media/logo.5d5d9eef.svg"
+  }
+]);
