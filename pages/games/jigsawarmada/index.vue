17633d506796c571cc55d12f18628907b09b5cda
Wes Ehrlichman <wes@superwes.com>
2020-09-12 13:43:59 -0500

    Add button linking to instructions for now - Wes

--- pages/games/jigsawarmada/index.vue
+++ pages/games/jigsawarmada/index.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="content">
-    <h1 class="text-xl font-bold text-white text-center mt-4">Jigsaw Armada</h1>
-    <img class="BoxTop" src="~/assets/JigsawArmada-boxtop.png" />
+    <div class="flex flex-col">
+      <h1 class="text-xl font-bold text-white text-center mt-4">Jigsaw Armada</h1>
+      <img class="BoxTop" src="~/assets/JigsawArmada-boxtop.png" />
+
+      <div class="text-center mt-3">
+        <a
+          href="JigsawArmadaInstructions.pdf"
+          target="_blank"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+        >Jigsaw Armada Instructions</a>
+      </div>
+    </div>
   </div>
 </template>
 
