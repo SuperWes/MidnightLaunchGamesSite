35ef1c89c98b2e285012688c635356b14a2cec83
Wes Ehrlichman <wes@superwes.com>
2020-09-13 15:54:48 -0500

    Refactor css - Wes

--- components/AppLinks.vue
+++ components/AppLinks.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="-mx-4 md:flex md:items-center">
-    <a
-      href="/"
-      class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"
-    >About</a>
-    <a
-      href="/games"
-      class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"
-    >Games</a>
-    <a
-      href="/contact"
-      class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"
-    >Contact</a>
+    <a href="/" class="linkOption mx-4 mt-2 md:mt-0">About</a>
+    <a href="/games" class="linkOption mx-4 mt-2 md:mt-0">Games</a>
+    <a href="/contact" class="linkOption mx-4 mt-2 md:mt-0">Contact</a>
     <a
       href="http://www.twitter.com/MidnightLGs"
+      class="linkOption mx-4 mt-2 md:mt-0"
       target="_blank"
-      class="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600"
     >Twitter</a>
   </div>
 </template>
+
+<style scoped>
+@layer base {
+  .linkOption {
+    @apply block;
+    @apply text-sm;
+    @apply text-gray-700;
+    @apply capitalize;
+  }
+
+  .linkOption:hover {
+    @apply text-blue-600;
+  }
+}
+</style>
