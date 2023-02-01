a3723c38bb87b5e2703f4a99b2abfe51dc8b22be
Wes Ehrlichman <wes@superwes.com>
2023-02-01 00:33:40 -0600

    Added a contact form - WE

---
+++ pages/contact/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='content'>
+    <h1 class="pageTitle mt-4">Contact</h1>
+   <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeLD46wukfuUSvChVXO34tKCNMjljqNB5nhleFStzZv6GWVww/viewform?embedded=true" width="640" height="709" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+  </div>
+
+</template>
+
+
+<style scoped>
+.content {
+  margin: auto;
+}
+
+h1.pageTitle {
+  @apply text-xl;
+  @apply font-bold;
+  @apply text-white;
+  @apply text-center;
+}
+
+</style>
