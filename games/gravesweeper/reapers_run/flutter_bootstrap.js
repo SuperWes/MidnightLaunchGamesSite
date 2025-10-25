bc91249600693494f715561ea5afbaa21554c945
Wes Ehrlichman <wes@superwes.com>
2025-10-25 17:40:59 -0500

    Updates

--- games/gravesweeper/reapers_run/flutter_bootstrap.js
+++ games/gravesweeper/reapers_run/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1690226776"
+    serviceWorkerVersion: "3939846154"
   }
 });
