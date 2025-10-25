3ffa5557457e309065dffdb9768f80c5d54696f5
Wes Ehrlichman <wes@superwes.com>
2025-10-25 16:38:56 -0500

    Fix migration of data - WE
    maybe fix save issue?
    
    hopefully fix web errors - WE
    
    Remove firebase from web - WE
    
    Update now that the game works on web again - WE
    
    I was deploying the real game. Nooo - WE

--- static/games/gravesweeper/reapers_run/flutter_bootstrap.js
+++ static/games/gravesweeper/reapers_run/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1987743551"
+    serviceWorkerVersion: "3768763858"
   }
 });
