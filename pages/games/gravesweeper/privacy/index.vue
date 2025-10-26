15a7696649836289d13b416d8ce479749451f2f6
Wes Ehrlichman <wes@superwes.com>
2025-10-25 21:15:42 -0500

    Add support page link to privacy policy for better UX

--- pages/games/gravesweeper/privacy/index.vue
+++ pages/games/gravesweeper/privacy/index.vue
@@ -127,13 +127,23 @@
               Contact Page
             </nuxt-link>
           </p>
+          <p class="mt-3">
+            For game support and FAQs, visit our 
+            <nuxt-link to="/games/gravesweeper/support" class="text-blue-400 hover:text-blue-300 underline">
+              Support Page
+            </nuxt-link>
+          </p>
         </section>
 
-        <div class="text-center mt-8 mb-4">
+        <div class="text-center mt-8 mb-4 flex gap-3 justify-center">
           <nuxt-link
             to="/games/gravesweeper"
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
           >Back to Gravesweeper</nuxt-link>
+          <nuxt-link
+            to="/games/gravesweeper/support"
+            class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded"
+          >Get Support</nuxt-link>
         </div>
       </div>
     </div>
