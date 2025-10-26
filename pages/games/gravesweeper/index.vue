2e37ef8a67dcd0d27e590a9a7567ab5c204c4b88
Wes Ehrlichman <wes@superwes.com>
2025-10-26 08:35:48 -0500

    iOS Link works - WE

--- pages/games/gravesweeper/index.vue
+++ pages/games/gravesweeper/index.vue
@@ -17,9 +17,11 @@
             class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-6 border border-purple-700 rounded"
           >🎮 Play Web Demo</a>
           
-          <div class="bg-gray-600 text-white font-bold py-2 px-4 border border-gray-700 rounded">
-            Coming Soon to iOS
-          </div>
+          <a
+            href="https://apps.apple.com/us/app/gravesweeper/id6754204296"
+            target="_blank"
+            class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-6 border border-blue-700 rounded"
+          >📱 Download on iOS</a>
           
           <div class="bg-gray-600 text-white font-bold py-2 px-4 border border-gray-700 rounded">
             Coming Soon to Android
