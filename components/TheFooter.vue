2a2692f82236da3987eb8717665ff072c9fb7f03
Wes Ehrlichman <wes@superwes.com>
2025-12-21 13:52:41 -0600

    update footer - WE

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="bg-white mt-3">
       <div class="container mx-auto px-6 py-3 flex flex-wrap items-center justify-between">
-        <div class="text-xs text-gray-600">© 2020 Midnight Launch Games</div>
+        <div class="text-xs text-gray-600">&copy; {{ new Date().getFullYear() }} Midnight Launch Games</div>
         <div class="text-xs text-gray-600">Site design and development by Wes Ehrlichman</div>
       </div>
     </footer>
