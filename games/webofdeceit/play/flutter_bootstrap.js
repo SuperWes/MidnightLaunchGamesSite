59391651367b031e6fe6a6544a2dbe10cbc1d949
Wes Ehrlichman <wes@superwes.com>
2026-02-01 09:21:38 -0600

    Updates

--- games/webofdeceit/play/flutter_bootstrap.js
+++ games/webofdeceit/play/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3610386345"
+    serviceWorkerVersion: "730225864"
   }
 });
