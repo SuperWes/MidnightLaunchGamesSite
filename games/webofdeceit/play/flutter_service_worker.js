627e1602bda955c3471f7fe5f846b0e2283c6470
Wes Ehrlichman <wes@superwes.com>
2026-02-01 15:42:58 -0600

    Updates

--- games/webofdeceit/play/flutter_service_worker.js
+++ games/webofdeceit/play/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3548456ddd6d2b68432e32c36229b3c3",
+const RESOURCES = {"flutter_bootstrap.js": "20844d4476f730eaf760ef325175ecbb",
 "version.json": "e67133c6090177276769df774ab943a2",
 "index.html": "60157fd6f3d0e982fa3ed2e7af8a39bf",
 "/": "60157fd6f3d0e982fa3ed2e7af8a39bf",
-"main.dart.js": "73da0212b4ae7738716856c0729fe4a9",
+"main.dart.js": "79c98146d102eb16b41191bf718142da",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "57b5c86168fb51ec04a2be2c8f6ad0db",
 "icons/Icon-192.png": "a9c8e187c180d49eadb95af6c65341da",
